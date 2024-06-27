41ca69c785823e8235441ce28f49f91a2fb38e38
isai hernandez <isaidj1999@gmail.com>
2024-06-26 20:28:48 -0500

    /categories error catch

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -10,6 +10,6 @@ export async function GET(request: Request) {
 
     return Response.json({ data });
   } catch (error) {
-    return Response.error();
+    return Response.json({ error });
   }
 }
