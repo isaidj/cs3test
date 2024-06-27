5474549c4f25b3a8d99439ca26f4805a7edcec2d
isai hernandez <isaidj1999@gmail.com>
2024-06-26 20:32:22 -0500

    test rendering /

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import TableComponent from "../components/TableComponent";
 
 const CategoryPage = ({ params }: { params: { slug: string } }) => {
-  return <TableComponent idCategory={params.slug} />;
+  // return <TableComponent idCategory={params.slug} />;
+  return <div></div>;
 };
 
 export default CategoryPage;
